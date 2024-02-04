a717cfb1802c4fbf82d198716f925bb197101e9e
Kozakilpr <ilpr@ukr.net>
2024-02-04 21:05:31 +0100

    Lesson13 Massiv HomeWork

---
+++ src/lesson13Massiv/homeWork/Task2.java
@@ -0,0 +1,11 @@
+package lesson13Massiv.homeWork;
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] Task1 = {44, 67, 9, 11, 34, 78};
+
+        System.out.println(Arrays.toString(Task1));
+
+    }}
